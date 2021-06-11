6ade8a132f2ed8e5e2e3e23db4199f51f3b035dd
unknown <kevin.maulana@erloom.id>
2021-06-11 19:20:40 +0700

    Template Login Register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,69 +1,271 @@
+<body class="align">
 
-<body class="bg-gradient-primary">
-
-    <div class="container">
-
-        <!-- Outer Row -->
-        <div class="row justify-content-center">
-
-            <div class="col-lg-7">
-
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            <div class="col-lg">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Register Page</h1>
-                                    </div>
-
-                                    <form class="user" method="POST" action="{{url('auth/register')}}">
-                                        @csrf
-                                        <div class="form-group">
-                                            <input type="email" class="form-control form-control-user @error('email') is-invalid @enderror" id="email" name='email' aria-describedby="emailHelp" placeholder="Enter Email Address..." value="" >
-                                                @error('email')
-                                                <div class="invalid-feedback">{{ $message }}</div>
-                                                  @enderror
-                                              </div>
-                                        </div>
-
-
-                                        <div class="form-group">
-                                            <input type="password" class="form-control form-control-user @error('password') is-invalid @enderror" id="password" name='password' placeholder="Password">
-                                        </div>
-                                        @error('password')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                          @enderror
-                                      </div>
-
-
-
-                                        <div class="form-group">
-                                            <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Remember Me</label>
-                                            </div>
-                                        </div>
-                                        <button type="submit" class="btn btn-primary btn-user btn-block">
-                                            Register
-                                        </button>
-                                    </form>
-                                    <hr>
-                                    <div class="text-center">
-                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
-                                    </div>
-                                    <div class="text-center">
-                                        <a class="small" href="">Create an Account!</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
+    <div class="grid">
+  
+      <form action="{{url('auth/register')}}" method="POST" class="form login">
+        @csrf
+        <h1 class="h4 text-gray-900 mb-4" style="text-align: center;">Register Page</h1>
+     
+        @if(session('message'))
+        <div class="alert alert-success" style="text-align: center">
+            {{session('message')}}
         </div>
-
+        @endif
+        <div class="form__field">
+          <label for="register_email"><svg class="icon">
+              <use xlink:href="#icon-user"></use>
+            </svg><span class="hidden">Email</span></label>
+          <input autocomplete="email" id="register_email" type="text" name="email" class="form__input" placeholder="Email" required>
+        </div>
+  
+        <div class="form__field">
+          <label for="register_password"><svg class="icon">
+              <use xlink:href="#icon-lock"></use>
+            </svg><span class="hidden">Password</span></label>
+          <input id="register_password" type="password" name="password" class="form__input" placeholder="Password" required>
+        </div>
+  
+        <div class="form__field">
+          <input type="submit" value="Register">
+        </div>
+  
+      </form>
+  
+      <p class="text--center">You have a member? <a href="{{url('auth/login')}}">Sign in now</a> <svg class="icon">
+          <use xlink:href="#icon-arrow-right"></use>
+        </svg></p>
+  
     </div>
+  
+    <svg xmlns="http://www.w3.org/2000/svg" class="icons">
+      <symbol id="icon-arrow-right" viewBox="0 0 1792 1792">
+        <path d="M1600 960q0 54-37 91l-651 651q-39 37-91 37-51 0-90-37l-75-75q-38-38-38-91t38-91l293-293H245q-52 0-84.5-37.5T128 1024V896q0-53 32.5-90.5T245 768h704L656 474q-38-36-38-90t38-90l75-75q38-38 90-38 53 0 91 38l651 651q37 35 37 90z" />
+      </symbol>
+      <symbol id="icon-lock" viewBox="0 0 1792 1792">
+        <path d="M640 768h512V576q0-106-75-181t-181-75-181 75-75 181v192zm832 96v576q0 40-28 68t-68 28H416q-40 0-68-28t-28-68V864q0-40 28-68t68-28h32V576q0-184 132-316t316-132 316 132 132 316v192h32q40 0 68 28t28 68z" />
+      </symbol>
+      <symbol id="icon-user" viewBox="0 0 1792 1792">
+        <path d="M1600 1405q0 120-73 189.5t-194 69.5H459q-121 0-194-69.5T192 1405q0-53 3.5-103.5t14-109T236 1084t43-97.5 62-81 85.5-53.5T538 832q9 0 42 21.5t74.5 48 108 48T896 971t133.5-21.5 108-48 74.5-48 42-21.5q61 0 111.5 20t85.5 53.5 62 81 43 97.5 26.5 108.5 14 109 3.5 103.5zm-320-893q0 159-112.5 271.5T896 896 624.5 783.5 512 512t112.5-271.5T896 128t271.5 112.5T1280 512z" />
+      </symbol>
+    </svg>
+  
+  </body>
+
+    <style>
+        @use postcss-preset-env {
+            stage: 0;
+        }
+        
+        /* config.css */
+        
+        :root {
+            --baseColor: #606468;
+        }
+        
+        /* helpers/align.css */
+        
+        .align {
+            display: grid;
+            place-items: center;
+        }
+        
+        .grid {
+            inline-size: 90%;
+            margin-inline: auto;
+            max-inline-size: 25rem;
+        }
+        
+        /* helpers/hidden.css */
+        
+        .hidden {
+            border: 0;
+            clip: rect(0 0 0 0);
+            height: 1px;
+            margin: -1px;
+            overflow: hidden;
+            padding: 0;
+            position: absolute;
+            width: 1px;
+        }
+        
+        /* helpers/icon.css */
+        
+        :root {
+            --iconFill: var(--baseColor);
+        }
+        
+        .icons {
+            display: none;
+        }
+        
+        .icon {
+            block-size: 1em;
+            display: inline-block;
+            fill: var(--iconFill);
+            inline-size: 1em;
+            vertical-align: middle;
+        }
+        
+        /* layout/base.css */
+        
+        :root {
+            --htmlFontSize: 100%;
+        
+            --bodyBackgroundColor: #2c3338;
+            --bodyColor: var(--baseColor);
+            --bodyFontFamily: "Open Sans";
+            --bodyFontFamilyFallback: sans-serif;
+            --bodyFontSize: 0.875rem;
+            --bodyFontWeight: 400;
+            --bodyLineHeight: 1.5;
+        }
+        
+        * {
+            box-sizing: inherit;
+        }
+        
+        html {
+            box-sizing: border-box;
+            font-size: var(--htmlFontSize);
+        }
+        
+        body {
+            background-color: var(--bodyBackgroundColor);
+            color: var(--bodyColor);
+            font-family: var(--bodyFontFamily), var(--bodyFontFamilyFallback);
+            font-size: var(--bodyFontSize);
+            font-weight: var(--bodyFontWeight);
+            line-height: var(--bodyLineHeight);
+            margin: 0;
+            min-block-size: 100vh;
+        }
+        
+        /* modules/anchor.css */
+        
+        :root {
+            --anchorColor: #eee;
+        }
+        
+        a {
+            color: var(--anchorColor);
+            outline: 0;
+            text-decoration: none;
+        }
+        
+        a:focus,
+        a:hover {
+            text-decoration: underline;
+        }
+        
+        /* modules/form.css */
+        
+        :root {
+            --formGap: 0.875rem;
+        }
+        
+        input {
+            background-image: none;
+            border: 0;
+            color: inherit;
+            font: inherit;
+            margin: 0;
+            outline: 0;
+            padding: 0;
+            transition: background-color 0.3s;
+        }
+        
+        input[type="submit"] {
+            cursor: pointer;
+        }
+        
+        .form {
+            display: grid;
+            gap: var(--formGap);
+        }
+        
+        .form input[type="password"],
+        .form input[type="text"],
+        .form input[type="submit"] {
+            inline-size: 100%;
+        }
+        
+        .form__field {
+            display: flex;
+        }
+        
+        .form__input {
+            flex: 1;
+        }
+        
+        /* modules/login.css */
+        
+        :root {
+            --loginBorderRadus: 0.25rem;
+            --loginColor: #eee;
+        
+            --loginInputBackgroundColor: #3b4148;
+            --loginInputHoverBackgroundColor: #434a52;
+        
+            --loginLabelBackgroundColor: #363b41;
+        
+            --loginSubmitBackgroundColor: #ea4c88;
+            --loginSubmitColor: #eee;
+            --loginSubmitHoverBackgroundColor: #d44179;
+        }
+        
+        .login {
+            color: var(--loginColor);
+        }
+        
+        .login label,
+        .login input[type="text"],
+        .login input[type="password"],
+        .login input[type="submit"] {
+            border-radius: var(--loginBorderRadus);
+            padding: 1rem;
+        }
+        
+        .login label {
+            background-color: var(--loginLabelBackgroundColor);
+            border-bottom-right-radius: 0;
+            border-top-right-radius: 0;
+            padding-inline: 1.25rem;
+        }
+        
+        .login input[type="password"],
+        .login input[type="text"] {
+            background-color: var(--loginInputBackgroundColor);
+            border-bottom-left-radius: 0;
+            border-top-left-radius: 0;
+        }
+        
+        .login input[type="password"]:focus,
+        .login input[type="password"]:hover,
+        .login input[type="text"]:focus,
+        .login input[type="text"]:hover {
+            background-color: var(--loginInputHoverBackgroundColor);
+        }
+        
+        .login input[type="submit"] {
+            background-color: var(--loginSubmitBackgroundColor);
+            color: var(--loginSubmitColor);
+            font-weight: 700;
+            text-transform: uppercase;
+        }
+        
+        .login input[type="submit"]:focus,
+        .login input[type="submit"]:hover {
+            background-color: var(--loginSubmitHoverBackgroundColor);
+        }
+        
+        /* modules/text.css */
+        
+        p {
+            margin-block: 1.5rem;
+        }
+        
+        .text--center {
+            text-align: center;
+        }
+      
+    </style>
